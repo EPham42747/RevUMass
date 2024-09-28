cb8924dc764b3705319c72568b50280fbe27a374
D-SehKim <123012677+D-SehKim@users.noreply.github.com>
2024-09-28 15:54:00 -0400

    Added picturebox functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from "react";
 import PictureBox from './compontents/PictureBox'; // Import PictureBox component
-import HamburgerMenu from "./compontents/HamburgerMenu"; // Import HamburgerMenu component
 import "./styles/App.css"; // Make sure to import your CSS
 
 function App() {
   const [items, setItems] = useState([]);
 
-  // Fetch data from MongoDB API
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response_food = await fetch('http://172.31.128.6:3001/food/');
-        const response_reviews = await fetch('http://172.31.128.6:3001/review/66eb1b2537e8362ce2bb7106/'); // Assume you have an API endpoint to fetch MongoDB data
-        const data_food = await response_food.json();
-        const data_reviews = await response_reviews.json();
-        console.log(data_food);
-        console.log(data_reviews);
-        setItems(data_food); // Set data to state
-      } catch (error) {
-        console.error("Error fetching data HELLO: ", error);
-      }
-    };
-
-    fetchData();
+    fetchData("food");
   }, []);
+
+  // Fetch data from MongoDB API
+  const fetchData = async (table) => {
+    try {
+      const response = await fetch(`http://172.31.128.6:3001/${table}/`);
+      const data = await response.json();
+      setItems(data); // Set data to state
+    } catch (error) {
+      console.error("Error fetching data Error Code 512930102392190321: ", error);
+    }
+  };
+
+  const handleClick = (event) => {
+    const buttonText = event.target.id;
+    try {
+      fetchData(buttonText);
+    } catch (error) {
+      console.error("There was a minor inconvianve Fetching data: ", error);
+    }
+  };
+
 
   return (
     <div className="App">
@@ -37,12 +42,14 @@ function App() {
         </div>
         <div className="mobile-layout">
           {/* Mobile specific layout components */}
-          <h1>RevUMass</h1>
-          <nav>
-            <button>Food</button>
-            <button>Dorms</button>
-            <button>Study Spots</button>
-          </nav>
+          <section className="fixed_header">
+            <h1>RevUMass</h1>
+            <nav className="button-container">
+              <button onClick={handleClick} id="food">Food</button>
+              <button onClick={handleClick} id="dorms">Dorms</button>
+              <button onClick={handleClick} id="studySpots">Study Spots</button>
+            </nav>
+          </section>
           <section className="picture-box-container">
             {items.map((item, index) => (
               <PictureBox
@@ -52,6 +59,48 @@ function App() {
                 description={item._id}
               />
             ))}
+            <PictureBox
+                key="0"
+                image="/test_img.jpg"
+                title="Fish Tacos"
+                description="Description"
+              />
+              <PictureBox
+                key="1"
+                image="/test_img.jpg"
+                title="Taco Casserole"
+                description="Description"
+              />
+              <PictureBox
+                key="2"
+                image="/test_img.jpg"
+                title="Pizza (Cheese)"
+                description="Description"
+              />
+              <PictureBox
+                key="3"
+                image="/test_img.jpg"
+                title="Pizza (Chicken)"
+                description="Description"
+              />
+              <PictureBox
+                key="4"
+                image="/test_img.jpg"
+                title="Ravioli"
+                description="Description"
+              />
+              <PictureBox
+                key="5"
+                image="/test_img.jpg"
+                title="Ramen"
+                description="Description"
+              />
+              <PictureBox
+                key="6"
+                image="/test_img.jpg"
+                title="Stir Fry"
+                description="Description"
+              />
           </section>
         </div>
       </main>
